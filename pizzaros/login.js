538bd1ca060fb00aef4a3984cefbe7f8365417c4
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2021-11-07 10:32:42 -0300

    melhorando script de login

--- pizzaros/login.js
+++ pizzaros/login.js
@@ -1,11 +1,13 @@
 var entra = document.getElementById("entra")
+var senhau
+var btnlogin
 
 function logar(){
-    var btnlogin = document.getElementById("btnlogin")
+    btnlogin = document.getElementById("btnlogin")
 
 
     var nome = document.getElementById("usuario").value 
-    var senhau = document.getElementById("senha-usuario")
+    senhau = document.getElementById("senha-usuario")
     var senha = senhau.value
 
     if(nome == '' || senha == ''){
@@ -16,6 +18,7 @@ function logar(){
     else{
         document.getElementById("usuario").value = 'Logado'
         document.getElementById("usuario").setAttribute("disabled", "true")
+        document.getElementById("usuario").setAttribute("id","loga")
 
         var btnsair = document.createElement("button")
         btnsair.innerHTML = 'Sair'
@@ -31,19 +34,12 @@ function logar(){
 
 function sair(){
     
-    document.getElementById("usuario").value = ''
-    document.getElementById("usuario").removeAttribute("disabled")
+    document.getElementById("loga").value = ''
+    document.getElementById("loga").removeAttribute("disabled")
+    document.getElementById("loga").setAttribute("id","usuario")
     
-    var sen = document.createElement("input","password")
-    sen.setAttribute("placeholder","Senha:")
-    sen.setAttribute("id","senha-usuario")
-    entra.appendChild(sen)
-
-    var botaologin = document.createElement("button")
-    botaologin.setAttribute("onclick", "logar()")
-    botaologin.setAttribute("id", "btnlogin")
-    botaologin.innerHTML = 'Login'
-    entra.appendChild(botaologin)
+    entra.appendChild(senhau)
+    entra.appendChild(btnlogin)
     
     var s = document.getElementById("btnsaiu")
     entra.removeChild(s)
